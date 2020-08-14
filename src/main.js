864ac60115b46ba1094ad6c180505d90050faac3
wangqs <gggyhyl@163.com>
2020-08-14 10:59:08 +0800

    路由守卫

--- src/main.js
+++ src/main.js
@@ -5,6 +5,25 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+// 守卫
+router.beforeEach((to, from, next) => {
+  if (window.sessionStorage.getItem('userToken')) {
+    next()
+  } else {
+    if (to.name === 'sort') {
+      next({
+        path: '/compress-img',
+        query: {
+          id: 111111
+        }
+      })
+    } else {
+      next()
+    }
+  }
+})
+
+
 new Vue({
   router,
   store,
