c1abf404402494d8b77bc815020118ff3cea8940
wangqs <gggyhyl@163.com>
2020-08-26 16:23:33 +0800

    target

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
         proxy: {
             [process.env.VUE_APP_BASIC_API]: {
                 logLevel:"debug",
-                target: 'http://gzstoreeme.ctwenhuayun.cn:8090/jiaozuo/web',
+                target: `http://gzstoreeme.ctwenhuayun.cn:8090/jiaozuo/web`,
                 // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
                 changeOrigin: true, 
                 ws: true,
