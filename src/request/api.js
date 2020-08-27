fa00998a9c5cf73e359aa860d38de27266024523
wangqs <gggyhyl@163.com>
2020-08-27 16:41:10 +0800

    配置接口修改

--- src/request/api.js
+++ src/request/api.js
@@ -1,5 +1,7 @@
 import {post} from "./http"
 
 export const getUserInfo = ({ userName, userPwd }) => {
-    return post('/apiTerminalUser/login.do', { userName, userPwd })
+    return post('/wxApplet/appletVideoList.do',{ userId: '84651f1ca50948e2ab6b98a449375ad4',
+    page: 1,
+    rows: 10})
 }
