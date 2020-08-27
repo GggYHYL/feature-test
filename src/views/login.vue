5d3fb08fd0d120c5d32b868e9e99895d7a976164
wangqs <gggyhyl@163.com>
2020-08-27 18:10:48 +0800

    登录修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -5,16 +5,16 @@
                 v-model="ruleForm.username"
                 name="用户名"
                 label="用户名"
-                placeholder="用户名"
-                :rules="[{ required: true, message: '请填写用户名' }]"
+                placeholder="请输入用户名"
+                :rules="[{ required: true, message: '请输入用户名' }]"
             />
             <van-field
                 v-model="ruleForm.userPwd"
                 type="password"
                 name="密码"
                 label="密码"
-                placeholder="密码"
-                :rules="[{ required: true, message: '请填写密码' }]"
+                placeholder="请输入密码"
+                :rules="[{ required: true, message: '请输入密码' }]"
             />
             <div>
                 <van-button round block type="info" native-type="submit">提交</van-button>
@@ -43,14 +43,7 @@ export default {
     },
 
     methods:{
-        onSubmit(formName){
-            this.$refs[formName].validate((valid) => {
-                if(valid){
-                }else {
-                    return null
-                }
-
-            })
+        onSubmit(){
         },
     }
 }
