ca78ac4971999287355c2a9c7f295dc9567764ab
wangqs <gggyhyl@163.com>
2020-08-27 14:35:04 +0800

    渐变

--- src/views/canvas.vue
+++ src/views/canvas.vue
@@ -3,32 +3,40 @@
 </template>
 
 <script>
-export default {
-    name: 'Canvas',
-    data(){
-        return ({
+    export default {
+        name: 'Canvas',
+        data(){
+            return ({
 
-        })
-    },
-    mounted(){
-        const canvas = this.$refs.canvas
-        let ctx = canvas.getContext('2d')
-        // canvas.width = 1000
-        // canvas.height = 1000
-        ctx.moveTo(100,100)
-        ctx.lineTo(100,300)
-        ctx.lineTo(300,300)
-        ctx.lineTo(300,100)
-        ctx.lineTo(100,100)
-        ctx.strokeStyle = 'green';
-        ctx.lineWidth = 1
-        ctx.stroke()
-        ctx.fillStyle = 'red'
-        ctx.fillRect(100,100,200,200)
-        // ctx.clearRect(0,0,canvas.width,canvas.height)
-    },
-    methods: {
+            })
+        },
+        mounted(){
+            const canvas = this.$refs.canvas
+            const ctx = canvas.getContext('2d')
+            // 渐变
+            const _linerGradient = ctx.createLinearGradient(0,0,200,100)
+            _linerGradient.addColorStop(0.4,'red')
+            _linerGradient.addColorStop(0.7,'green')
+            _linerGradient.addColorStop(1,'yellow')
+            ctx.fillStyle = _linerGradient
+            ctx.fillRect(100,100,200,200)
 
+
+            // 正方形
+            // ctx.moveTo(100,100)
+            // ctx.lineTo(100,300)
+            // ctx.lineTo(300,300)
+            // ctx.lineTo(300,100)
+            // ctx.lineTo(100,100)
+            // ctx.strokeStyle = 'green';
+            // ctx.lineWidth = 1
+            // ctx.stroke()
+            // ctx.fillStyle = 'red'
+            // ctx.fillRect(100,100,200,200)
+            // ctx.clearRect(0,0,canvas.width,canvas.height)
+        },
+        methods: {
+
+        }
     }
-}
 </script>
