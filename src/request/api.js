0479e4e979d4e784b407a5e5c7e804060d43c2a9
wangqs <gggyhyl@163.com>
2020-08-28 11:50:26 +0800

    axios拦截

--- src/request/api.js
+++ src/request/api.js
@@ -1,7 +1,7 @@
 import {post} from "./http"
 
 export const getUserInfo = ({ userName, userPwd }) => {
-    return post('/wxApplet/appletVideoList.do',{userId: '84651f1ca50948e2ab6b98a449375ad4',
+    return post('/wxApplet/appletVideoList.do',{userId: '',
         page: 1,
         rows: 10
     })
