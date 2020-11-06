967f94644acbc6ca80b49cf7e82d4479a214e2c8
wangqs <gggyhyl@163.com>
2020-11-06 16:50:09 +0800

    tablefenye

---
+++ src/views/table.vue
@@ -0,0 +1,275 @@
+<template>
+
+    <el-table
+        :data="tableData"
+        style="width: 100%"
+    >
+        <el-table-column
+            prop="questionNumber"
+            label="受理编号"
+            align="center"
+        >
+        </el-table-column>
+    </el-table>
+</template>
+<script>
+export default {
+    name: 'Table',
+    data(){
+        return ({
+            totalData:[
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+                {
+                    "questionNumber": "20201030010002",
+                },
+            ],
+            tableData:[]
+        })
+    },
+    created(){
+        
+    },
+    methods:{
+
+    }
+}
+</script>>
